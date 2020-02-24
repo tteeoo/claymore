20669acc07a49a4105792855d462959f46bd9842
Theo Henson <theodorehenson@protonmail.com>
2020-02-24 16:47:53 -0500

    Added verbosity for debugging

--- claymore/transmit.py
+++ claymore/transmit.py
@@ -5,18 +5,24 @@ index = 0
 
 def encrypt(message, password):
     global index
+    print("[XCRYPTION] index: {}".format(str(index)))
     iv = pbkdf2_hmac("sha256", bytes(password, "utf8"), (index*2).to_bytes(16, byteorder="big"), 200000, dklen=16).hex()
     key = pbkdf2_hmac("sha256", bytes(password, "utf8"), index.to_bytes(16, byteorder="big"), 200000, dklen=32).hex()
     obj = AES.new(key[0:32], AES.MODE_CFB, iv[0:16])
+    print("[XCRYPTION] key: {}, iv: {}".format(key, iv))
     ciphertext = obj.encrypt(message)
+    print("[XCRYPTION] cyphertext: {}".format(ciphertext))
     return ciphertext
 
 def decrypt(ciphertext, password):
     global index
+    print("[XCRYPTION] index: {}".format(str(index)))
     iv = pbkdf2_hmac("sha256", bytes(password, "utf8"), (index*2).to_bytes(16, byteorder="big"), 200000, dklen=16).hex()
     key = pbkdf2_hmac("sha256", bytes(password, "utf8"), index.to_bytes(16, byteorder="big"), 200000, dklen=32).hex()
     obj = AES.new(key[0:32], AES.MODE_CFB, iv[0:16])
+    print("[XCRYPTION] key: {}, iv: {}".format(key, iv))
     message = obj.decrypt(ciphertext)
+    print("[XCRYPTION] message: {}".format(message.decode("utf8")))
     return message
 
 def convert(m, p):
