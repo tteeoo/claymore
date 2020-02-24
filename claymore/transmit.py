b758f44c978265d98f377a37e972da4619550ee6
Theo Henson <theodorehenson@protonmail.com>
2020-02-23 20:46:26 -0500

    Added password hashing

--- claymore/transmit.py
+++ claymore/transmit.py
@@ -1,25 +1,35 @@
 from Crypto.Cipher import AES
+from hashlib import sha512
 
 pad = lambda s: s + (16 - len(s) % 16) * chr(16 - len(s) % 16) 
 unpad = lambda s : s[:-ord(s[len(s)-1:])]
+index = 0
 
-def encrypt(message, password, iv):
+def encrypt(message, password):
+    global index
+    iv = sha512((password + str(index*2)).encode("utf8")).digest() 
+    password = sha512((password + str(index)).encode("utf8")).digest() 
     obj = AES.new(password, AES.MODE_CFB, iv)
     ciphertext = obj.encrypt(message)
+    index += 1
     return ciphertext
 
 def decrypt(ciphertext, password, iv):
+    global index
+    iv = sha512((password + str(index*2)).encode("utf8")).digest() 
+    password = sha512((password + str(index)).encode("utf8")).digest() 
     obj = AES.new(password, AES.MODE_CFB, iv)
     message = obj.decrypt(ciphertext)
+    index += 1
     return message
 
 def convert(m, p):
-    m = encrypt(m, pad(p), pad("IV"))
+    m = encrypt(m, p)
 
     return m
 
 def unconvert(m, p):
-    m = decrypt(m, pad(p), pad("IV"))
+    m = decrypt(m, p)
     m = m.decode("utf8")
 
     return m
