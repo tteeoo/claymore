34e7f3835607faf4a97c99bf33047c98d75a963e
Theo Henson <theodorehenson@protonmail.com>
2020-02-23 22:30:31 -0500

    Attempting to fix encryption

--- claymore/transmit.py
+++ claymore/transmit.py
@@ -1,8 +1,6 @@
 from Crypto.Cipher import AES
 from hashlib import sha512
 
-pad = lambda s: s + (16 - len(s) % 16) * chr(16 - len(s) % 16) 
-unpad = lambda s : s[:-ord(s[len(s)-1:])]
 index = 0
 
 def encrypt(message, password):
@@ -11,7 +9,6 @@ def encrypt(message, password):
     password = sha512((password + str(index)).encode("utf8")).digest() 
     obj = AES.new(password[0:32], AES.MODE_CFB, iv[0:16])
     ciphertext = obj.encrypt(message)
-    index += 1
     return ciphertext
 
 def decrypt(ciphertext, password):
@@ -20,16 +17,19 @@ def decrypt(ciphertext, password):
     password = sha512((password + str(index)).encode("utf8")).digest() 
     obj = AES.new(password[0:32], AES.MODE_CFB, iv[0:16])
     message = obj.decrypt(ciphertext)
-    index += 1
     return message
 
 def convert(m, p):
+    global index
     m = encrypt(m, p)
+    index += 1
 
     return m
 
 def unconvert(m, p):
+    global index
     m = decrypt(m, p)
     m = m.decode("utf8")
+    index += 1
 
     return m
