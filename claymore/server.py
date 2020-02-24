43dac186b4fa48c30a71b321a996b00bac93cb8f
Theo Henson <theodorehenson@protonmail.com>
2020-02-23 21:01:02 -0500

    Cleaned up server.py

--- claymore/server.py
+++ claymore/server.py
@@ -1,7 +1,7 @@
 from socket import socket, AF_INET, SOCK_STREAM
-from .transmit import unconvert
 from threading import Thread
-from sys import argv
+
+from .transmit import unconvert
 
 def main_loop(SERVER):
     print("[SERVER Thread] Main loop started")
@@ -10,18 +10,9 @@ def main_loop(SERVER):
         msg = client.recv(1024)
         print("[SERVER Thread] Recieved from {}: {}".format(client_address, unconvert(msg, "hell")))
 
-def broadcast(msg, prefix=""):  # prefix is for name identification.
-    """Broadcasts a message to all the clients."""
-    for sock in clients:
-        sock.send(bytes(prefix, "utf8")+msg)
-
-def server_init():
-    HOST = ""
-    PORT = int(argv[2])
-    BUFSIZ = 1024
-    ADDR = (HOST, PORT)
+def server_init(PORT):
     SERVER = socket(AF_INET, SOCK_STREAM)
-    SERVER.bind(ADDR)
+    SERVER.bind(("", PORT))
     SERVER.listen(1)
     Thread(target=main_loop, args=(SERVER,)).start()
 
