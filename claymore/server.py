2fdcc657fbf6f4e6aa6e4d2cadbd2c65be7ec581
Theo Henson <theodorehenson@protonmail.com>
2020-02-25 20:42:36 -0500

    Changed some message formatting server port stuff

--- claymore/server.py
+++ claymore/server.py
@@ -1,4 +1,4 @@
-from socket import socket, AF_INET, SOCK_STREAM
+from socket import socket, AF_INET, SOCK_STREAM, SO_REUSEADDR, SOL_SOCKET
 from threading import Thread
 from sys import exit, stderr
 
@@ -9,10 +9,11 @@ def main_loop(SERVER, PASS):
     client, client_address = SERVER.accept()
     while True:
         msg = client.recv(1024)
-        print("[SERVER Thread] Recieved from {}: {}".format(client_address, unconvert(msg, PASS)))
+        print("[SERVER Thread] Recieved from {}: {}".format(client_address[0], unconvert(msg, PASS)))
 
 def server_init(PORT, PASS):
     SERVER = socket(AF_INET, SOCK_STREAM)
+    SERVER.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
     try:
         SERVER.bind(("", PORT))
     except PermissionError:
